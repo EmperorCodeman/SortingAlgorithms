ec6c11f546cf6ceff851dc75602e9a80eb35f845
Alex <odetocodeman@gmail.com>
2017-12-14 15:53:48 -0700

    updated for order control

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,4 +1,4 @@
-def Bubble_Sort(collection, tabulations):
+def Bubble_Sort(collection, tabulations,leastToGreatest=True):
     """
     Sort contents of list by reference
         bubble sort is the least efficient sorting algorithm
@@ -6,11 +6,15 @@ def Bubble_Sort(collection, tabulations):
     """
     id = "bubbleSort"
     complete = False
+    if leastToGreatest:
+        order = "collection[i] > collection[i+1]"
+    else:
+        order = "collection[i] < collection[i+1]"
     while not complete:
         complete = True
         for i in range(len(collection)-1):
             tabulations[id]["comparisons"] += 1
-            if collection[i] > collection[i+1]:
+            if eval(order):
                 tabulations[id]["swaps"] += 1
                 collection[i], collection[i+1] = collection[i+1],collection[i]
                 complete = False
